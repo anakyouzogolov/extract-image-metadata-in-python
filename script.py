93a4ebefda2710a7b973693f332af365ff41e0cf
anakyouzogolov <64495296+anakyouzogolov@users.noreply.github.com>
2020-08-08 09:04:51 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+# read our image from current dir
+my_img = Image.open("moscow.jpg")
+
+# extract exif data
+exif_data = my_img.getexif()
+
+
+# iterrating over all exif data
+for tagId in exif_data:
+    # get the tag name
+    tag = TAGS.get(tagId, tagId)
+    data = exif_data.get(tagId)
+
+    print(f"{tag:16}: {data}")
